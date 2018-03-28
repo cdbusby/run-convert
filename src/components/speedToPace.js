8296e4e3895a79c150333fa266aef2ec907db17a
Colin Busby <cdbusby@gmail.com>
2018-03-28 14:49:59 -0300

    Fixing logic for converting decimal to minutes and seconds.

--- src/components/speedToPace.js
+++ src/components/speedToPace.js
@@ -1,23 +1,35 @@
-const speedToPace = function( speed, unit ) {
-    
+/**
+ * Convert speed of given unit to pace of any unit
+ * @param {Integer} speed 
+ * @param {*} FromUnit 
+ * @param {*} toUnit 
+ */
+const speedToPace = function (speed, fromUnit, toUnit) {
+
     // Get decimal version of pace and convert to string
     var paceInDec = 1 / (speed / 60);
     var paceInDecRounded = paceInDec.toFixed(2);
-    var paceAsString = paceInDecRounded.toString();
 
-    // Store minutes and seconds (dec) separately
-    var paceMinutes = paceAsString.split(".")[0];
-    var paceSeconds = paceAsString.split(".")[1];
-
-    // Convert seconds as decimal to seconds
-    var decimalInSec = 60 * paceSeconds;
-    var zerodDecInSec = `0.${decimalInSec}`;
-    var roundedDecInSec = parseFloat(zerodDecInSec).toFixed(2).split(".")[1];
-
-    // Add converted portion to the orifinal
-    var finalPace = `${paceMinutes}:${roundedDecInSec}`;
+    // Run decimal through function for time conversion
+    var finalPace = decimalToTime(paceInDecRounded);
 
     return finalPace;
 };
+
+/**
+ * Convert decimal to time
+ * @param {Integer} decimal 
+ */
+function decimalToTime(decimal) {
+
+    // Get the number of minutes
+    var min = Math.floor(Math.abs(decimal));
+
+    // Get the number of seconds
+    var sec = Math.floor((Math.abs(decimal) * 60) % 60);
+
+    // Jam them together and format as time
+    return `${(min < 10 ? "0" : "")}${min}:${(sec < 10 ? "0" : "")}${sec}`;
+}
 
 export default speedToPace;
